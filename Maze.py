860053bc407462d6dc6f149c7d1a8fb0ae4e99b3
Arrow338 <92538313+Arrow338@users.noreply.github.com>
2021-11-18 12:38:36 -0500

    Update Maze.py

--- Maze.py
+++ Maze.py
@@ -1,4 +1,3 @@
-import os
 import pygame
 from math import sin, radians, degrees, copysign
 from pygame.math import Vector2
@@ -11,19 +10,31 @@ class Maze:
         self.height = 900
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.exit = False
-
-    def run(self):
+        
+    
+    def drawBorder(self):
         while not self.exit:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.exit = True
             self.screen.fill((0, 0, 0))
             width = self.width
-            pygame.draw.line(self.screen, (255, 255, 255), (width, 3*width/4), (3*width/4, 3*width/4), 4)
+            pygame.draw.line(self.screen, (255, 255, 255), (0,0), (width, 0), 20)
+            pygame.draw.line(self.screen, (255, 255, 255), (0,0), (0, width), 20)
+            pygame.draw.line(self.screen, (255, 255, 255), (0,width), (width, width), 20)
+            pygame.draw.line(self.screen, (255, 255, 255), (width,width), (width, 0), 20)
+            pygame.display.flip()
+
+    def drawPath(self):
+        while not self.exit:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.exit = True
+            width = self.width
             pygame.display.flip()
 
     pygame.quit()
 
 if __name__ == '__main__':
     maze = Maze()
-    maze.run()
+    maze.drawBorder()
