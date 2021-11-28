1dd2157268851a83e8179d8da47b129ee21cc9e5
Arrow338 <92538313+Arrow338@users.noreply.github.com>
2021-11-27 23:57:01 -0500

    Update Maze1.py

--- Maze1.py
+++ Maze1.py
@@ -14,7 +14,7 @@ class Maze:
         self.height = 900
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.exit = False
-        self.lengths = [0, random.randint(50,300), random.randint(100,300), random.randint(75,200), random.randint(100,400), random.randint(30,50), random.randint(30,50), random.randint(30,50), random.randint(30,50), random.randint(30,50), random.randint(30,50),]
+        self.lengths = [0, random.randint(50,300), random.randint(100,300), random.randint(75,200), random.randint(100,400), random.randint(30,50), random.randint(200,400), random.randint(100,275), random.randint(50,300), random.randint(90,150), 700]
         self.prevX = 0
         self.prevY = 40
     
@@ -28,35 +28,48 @@ class Maze:
         pygame.draw.line(self.screen, (255, 255, 255), (0,width), (width, width), 20)
         pygame.draw.line(self.screen, (255, 255, 255), (width,width), (width, 0), 20)
 
-    def drawPath(self, screen):
+    def drawPath(self, x, y, rotate = 0):
         # for row in self.grid:
         #     for cell in row:
         #         cell.draw(screen)
         # l1 = Line()
         # l1.draw(screen)
-        self.drawLine(self.prevX, self.prevY, 1,0)
-        line1 = [self.prevX, self.prevY]
-        lines.append(line1)
-        self.drawLine(self.prevX, self.prevY, 0,2)
-        line2 = [self.prevX, self.prevY]
-        lines.append(line2)
-        self.drawLine(self.prevX, self.prevY, 3,0)
-        line3 = [self.prevX, self.prevY]
-        lines.append(line3)
-        self.drawLine(self.prevX, self.prevY, 0,4)
-        line4 = [self.prevX, self.prevY]
-        lines.append(line4)
-        self.drawLine(self.prevX, self.prevY, 5,0)
-        line5 = [self.prevX, self.prevY]
-        lines.append(line5)
-        self.drawLine(self.prevX, self.prevY, 0,6)
-        line6 = [self.prevX, self.prevY]
-        lines.append(line6)
+        self.prevX = x
+        self.prevY = y
 
-        print(lines)
+        if rotate == 0:
+            self.drawLine(self.prevX, self.prevY, 1,0)
+            self.drawLine(self.prevX, self.prevY, 0,2)
+            self.drawLine(self.prevX, self.prevY, 3,0)
+            self.drawLine(self.prevX, self.prevY, 0,4)
+            self.drawLine(self.prevX, self.prevY, 5,0)
+            self.drawLine(self.prevX, self.prevY, 0,6)
+            self.drawLine(self.prevX, self.prevY, 7,0)
+            self.drawLine(self.prevX, self.prevY, 0,8)
+            self.drawLine(self.prevX, self.prevY, 9,0)
+            self.drawLine(self.prevX, self.prevY, 0,10)
+        elif rotate == 90:
+            self.drawReflect(self.prevY, self.prevX, 0,1)
+            self.drawReflect(self.prevY, self.prevX, 2,0)
+            self.drawReflect(self.prevY, self.prevX, 0,3)
+            self.drawReflect(self.prevY, self.prevX, 4,0)
+            self.drawReflect(self.prevY, self.prevX, 0,5)
+            self.drawReflect(self.prevY, self.prevX, 6,0)
+            self.drawReflect(self.prevY, self.prevX, 0,7)
+            self.drawReflect(self.prevY, self.prevX, 8,0)
+            self.drawReflect(self.prevY, self.prevX, 0,9)
+            self.drawReflect(self.prevY, self.prevX, 10,0)
+
         
 
     def drawLine(self, x1, y1, lenx, leny):
+        x2 = x1+self.lengths[lenx]
+        y2 = y1+self.lengths[leny]
+        pygame.draw.line(self.screen, (255,0,0), (x1,y1), (x2,y2), 20)
+        self.prevX = x2
+        self.prevY = y2
+
+    def drawReflect(self, x1, y1, lenx,leny):
         x2 = x1+self.lengths[lenx]
         y2 = y1+self.lengths[leny]
         pygame.draw.line(self.screen, (255,0,0), (x1,y1), (x2,y2), 20)
@@ -88,7 +101,8 @@ if __name__ == '__main__':
     maze = Maze()
     while not maze.exit:
         maze.drawBorder()
-        maze.drawPath(maze.screen)
+        maze.drawPath(0, 40)
+        maze.drawPath(0, 40, 90)
         pygame.display.flip()
     
 
