ae68fc2e3fb6bf28f6328faf27948d339a785ee5
Arrow338 <92538313+Arrow338@users.noreply.github.com>
2021-11-27 23:14:51 -0500

    Update Maze1.py

--- Maze1.py
+++ Maze1.py
@@ -2,7 +2,11 @@ import pygame
 from pygame.math import Vector2
 import random   
 
+lines = []
+
 class Maze:
+
+
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Maze")
@@ -24,14 +28,33 @@ class Maze:
         pygame.draw.line(self.screen, (255, 255, 255), (0,width), (width, width), 20)
         pygame.draw.line(self.screen, (255, 255, 255), (width,width), (width, 0), 20)
 
-    # def drawPath(self):
-    #     self.drawLine(self.prevX, self.prevY, 1,0)
-    #     self.drawLine(self.prevX, self.prevY, 0,2)
-    #     self.drawLine(self.prevX, self.prevY, 3,0)
-    #     self.drawLine(self.prevX, self.prevY, 0,4)
-    #     self.drawLine(self.prevX, self.prevY, 5,0)
-    #     self.drawLine(self.prevX, self.prevY, 0,6)
+    def drawPath(self, screen):
+        # for row in self.grid:
+        #     for cell in row:
+        #         cell.draw(screen)
+        # l1 = Line()
+        # l1.draw(screen)
+        self.drawLine(self.prevX, self.prevY, 1,0)
+        line1 = [self.prevX, self.prevY]
+        lines.append(line1)
+        self.drawLine(self.prevX, self.prevY, 0,2)
+        line2 = [self.prevX, self.prevY]
+        lines.append(line2)
+        self.drawLine(self.prevX, self.prevY, 3,0)
+        line3 = [self.prevX, self.prevY]
+        lines.append(line3)
+        self.drawLine(self.prevX, self.prevY, 0,4)
+        line4 = [self.prevX, self.prevY]
+        lines.append(line4)
+        self.drawLine(self.prevX, self.prevY, 5,0)
+        line5 = [self.prevX, self.prevY]
+        lines.append(line5)
+        self.drawLine(self.prevX, self.prevY, 0,6)
+        line6 = [self.prevX, self.prevY]
+        lines.append(line6)
 
+        print(lines)
+        
 
     def drawLine(self, x1, y1, lenx, leny):
         x2 = x1+self.lengths[lenx]
@@ -40,21 +63,32 @@ class Maze:
         self.prevX = x2
         self.prevY = y2
     
-    def LegalMove(self, x):
-        return False
-
+    # def LegalMove(self, x):
+    #     return False
 
     pygame.quit()
 
-class Line:
-    def __init__(self, x1, y1, x2, y2, thickness = 20):
-        pygame.sprite.Sprite.__init__(self)
+# class Line:
+#     def __init__(self):
+#         pygame.sprite.Sprite.__init__(self)
+#         self.w = 20
+#         self.h = 20
+#         self.image = pygame.Surface([self.w, self.h],)
+#         self.image.fill((255, 0, 0))
+#         self.rect = self.image.get_rect()
+
+#     def draw(self, screen):
+#         line1 = self.rect
+#         screen.blit(self.image, line1)
+#         line2 = self.rect.move(20.0,20.0)
+#         screen.blit(self.image, line2)
+
 
 if __name__ == '__main__':
     maze = Maze()
     while not maze.exit:
-        # maze.drawPath()
         maze.drawBorder()
+        maze.drawPath(maze.screen)
         pygame.display.flip()
     
 
