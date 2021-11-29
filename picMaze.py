19c304e2ef3149e6fe940fe72a02e4a37d115975
Arrow338 <92538313+Arrow338@users.noreply.github.com>
2021-11-28 21:47:47 -0500

    Update picMaze.py

--- picMaze.py
+++ picMaze.py
@@ -20,13 +20,17 @@ class picMaze:
         self.screen.blit(image, (0, 0))
 
     def drawTurtle(self):
-        t = solver(0.0,40.0)
+        t = solver(0.0,0.0)
         current_dir = os.path.dirname(os.path.abspath(__file__))
         image_path = os.path.join(current_dir, "turtle.png")
         turtle_image = pygame.image.load(image_path)
-        while self.screen.get_at((int(t.position.x),int(t.position.y))) == (255,255,255):
+        while self.screen.get_at((int(t.position.x),int(t.position.y))) == (248, 248, 248, 255) or self.screen.get_at((int(t.position.x),int(t.position.y))) == (247, 247, 247, 255) :
+            # print(self.screen.get_at((int(t.position.x),int(t.position.y))))
             t.xIncrease()
+            # print(self.screen.get_at((int(t.position.x),int(t.position.y))))
             self.screen.blit(turtle_image, t.position)
+            
+            
 
     pygame.quit()
 
@@ -35,5 +39,6 @@ if __name__ == '__main__':
     while not maze.exit:
         maze.eventQueue()
         maze.pasteMaze()
+        maze.drawTurtle()
         pygame.display.flip()
 
