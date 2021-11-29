1ea7c055a615e4396f2cb0a2df97884827168afd
Arrow338 <92538313+Arrow338@users.noreply.github.com>
2021-11-28 20:22:01 -0500

    Update solver.py

--- solver.py
+++ solver.py
@@ -5,10 +5,15 @@ from collections import deque
 class solver:
     
     def __init__(self, m):
+        pygame.sprite.Sprite.__init__(self)
+        self.w = 20
+        self.h = 20
         self.maze=m
         self.path=deque()
         
     def move(self, x):
+        image_path = os.path.join(current_dir, "car.png")
+        car_image = pygame.image.load(image_path)
         if(self.maze.LegalMove(x)):
             self.path.append(x)
         else:
